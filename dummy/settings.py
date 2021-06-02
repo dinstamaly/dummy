22144aeeedb1e1a72d1a5db0c7a71aac0235a89e
Din <you@example.com>
2021-06-02 14:30:55 +0600

    test

--- dummy/settings.py
+++ dummy/settings.py
@@ -148,10 +148,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # celery
 # os.environ.get('SECRET_KEY')
-# BROKER_URL=os.environ['REDIS_URL'],
-# CELERY_RESULT_BACKEND=os.environ['REDIS_URL']
-BROKER_URL='redis://localhost:6379'
-CELERY_RESULT_BACKEND='redis://localhost:6379'
+BROKER_URL=os.environ['REDIS_URL'],
+CELERY_RESULT_BACKEND=os.environ['REDIS_URL']
+# BROKER_URL='redis://localhost:6379'
+# CELERY_RESULT_BACKEND='redis://localhost:6379'
 CELERY_TIMEZONE = "Asia/Bishkek"
 
 
