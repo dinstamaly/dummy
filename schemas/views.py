bb69ed469784c7f798f349b44af4450dc369b3ec
Din <you@example.com>
2021-06-04 13:01:23 +0600

    testtt8 path

--- schemas/views.py
+++ schemas/views.py
@@ -160,4 +160,5 @@ class FileDownloadView(LoginRequiredMixin, View):
 
     def get(self, request, dataset_id):
         data_set = get_object_or_404(DataSet, id=dataset_id)
-        return FileResponse(open(urls(data_set.file), 'rb'))
+        # return FileResponse(open(urls(data_set.file), 'rb'))
+        return FileResponse(data_set.file)
