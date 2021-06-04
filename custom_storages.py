3d53ad13e0239181cdfc5db517077e1d9a53e071
Din <you@example.com>
2021-06-04 20:55:33 +0600

    file conf test15

--- custom_storages.py
+++ custom_storages.py
@@ -2,7 +2,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
 
-    location = 'mediafiles'
+    # location = 'mediafiles'
 
     file_overwrite = False
 
